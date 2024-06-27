b99027e71810da98392041abc6e9851ff5add2d9
mkpage1337 <mrku1337@gmail.com>
2024-06-27 14:15:15 +0200

    styling footer

--- components/footer_.jsx
+++ components/footer_.jsx
@@ -6,15 +6,21 @@ import { BsFillTelephoneFill } from "react-icons/bs";
 
 export default function Footer() {
   const footerStyled =
-    "m-auto p-5 bg-neutral-800 text-neutral-100 flex gap-[20px] justify-center items-center";
-  const divWrapper = "w-1/3 flex flex-col gap-[20px] justify-center items-center";
-  const divIconElement = "flex justify-center items-center gap-[10px]";
-  const ulClasses = "flex flex-col gap-[10px] text-xl p-3";
-  const linkClasses = "hover:cursor hover:text-neutral-400";
+    "m-auto p-5 bg-neutral-800 text-neutral-100 flex gap-[20px] justify-around items-center max-[500px]:flex-col";
+
+  const ulClasses = "flex flex-col gap-[10px] p-3";
+  const linkClasses =
+    "text-xl max-md:text-xs hover:cursor hover:text-neutral-400";
+
+  const divBoxMiddle = "w-1/3 max-md:w-full";
+  const divBoxMiddleText = "max-md:text-xs";
+
+  const divBoxThird = "flex flex-col gap-[20px] ";
+  const divIconElement = "flex justify-center items-center gap-[10px] max-md:text-xs";
 
   return (
     <footer className={footerStyled}>
-      <div className={divWrapper}>
+      <nav>
         <ul className={ulClasses}>
           <Link className={linkClasses} href="/">
             Home
@@ -29,16 +35,16 @@ export default function Footer() {
             Contact
           </Link>
         </ul>
-      </div>
-      <div className={divWrapper}>
-        <p>
+      </nav>
+      <div className={divBoxMiddle}>
+        <p className={divBoxMiddleText}>
           Discover the unique charm and beauty of my original paintings, crafted
           with passion and creativity. Each piece is a vibrant expression of
           emotion and artistry, perfect for adding a touch of elegance to any
           space. Invest in art that speaks to the soul and transforms your home.
         </p>
       </div>
-      <div className={divWrapper}>
+      <div className={divBoxThird}>
         <div className={divIconElement}>
           <MdOutlineEmail />: @email
         </div>
