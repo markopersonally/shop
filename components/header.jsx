18ec722d738ef0bfb91e0a907c95cc635a61e9f2
mkpage1337 <mrku1337@gmail.com>
2024-06-27 15:28:52 +0200

    menu burger

--- components/header.jsx
+++ components/header.jsx
@@ -1,21 +1,62 @@
+"use client";
+import { useState } from "react";
+
 import Link from "next/link";
 import { FaPaintBrush } from "react-icons/fa";
 import { MdOutlineLocalGroceryStore } from "react-icons/md";
+import { TbMenuDeep } from "react-icons/tb";
 
 export default function Header() {
+  const [navMobile, setNavMobile] = useState(false);
+
+  const handleToggle = () => {
+    setNavMobile(!navMobile);
+  };
+
   const navClasses =
     "p-3 flex items-center justify-between bg-neutral-800 text-neutral-100";
   const ulClasses = "flex gap-[50px] text-2xl p-3";
   const linkClasses = " border-2 p-3 rounded-lg hover:bg-neutral-600";
   const logoClasses = "border-2 p-5 rounded-full cursor";
 
+  const burgerMenu = "text-[35px] cursor-pointer";
+  const mobileMenuClasses = navMobile ? "flex" : "hidden";
+  const navMobileClasses = "flex flex-col";
+
   return (
     <header>
-      <nav className={navClasses}>
+      {/* desktop */}
+      <nav className={`${navClasses} hidden lg:flex`}>
         <Link href="/" className={logoClasses}>
           <FaPaintBrush />
         </Link>
         <ul className={ulClasses}>
+          <Link className={linkClasses} href="/">
+            Home
+          </Link>
+          <Link className={linkClasses} href="/about">
+            About Me
+          </Link>
+          <Link className={linkClasses} href="/pictures">
+            Pictures
+          </Link>
+          <Link className={linkClasses} href="/contact">
+            Contact
+          </Link>
+          <Link className={linkClasses} href="/store">
+            <MdOutlineLocalGroceryStore />
+          </Link>
+        </ul>
+      </nav>
+      {/* tablet/mobile */}
+      <nav className={`${navClasses} flex-col lg:hidden`}>
+        <div className={navMobileClasses}>
+          <Link href="/" className={logoClasses}>
+            <FaPaintBrush />
+          </Link>
+          <TbMenuDeep onClick={handleToggle} className={burgerMenu} />
+        </div>
+        <ul className={mobileMenuClasses}>
           <Link className={linkClasses} href="/">
             Home
           </Link>
