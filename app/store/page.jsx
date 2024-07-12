88dc47148dd9e19583c827eb15c32a96909f211a
mkpage1337 <mrku1337@gmail.com>
2024-07-12 16:03:24 +0200

    update

--- app/store/page.jsx
+++ app/store/page.jsx
@@ -1,20 +1,10 @@
 import Section from "@/components/section.jsx";
 import H2 from "@/components/h2.jsx";
-import { useCart } from "@/context/CartContext";
 
 export default function Store() {
-  const { cart } = useCart();
-
   return (
     <Section>
       <H2>Your Cart</H2>
-      <ul>
-        {cart.map((item, index) => (
-          <li key={index}>
-            {item.title} - {item.price}
-          </li>
-        ))}
-      </ul>
     </Section>
   );
 }
